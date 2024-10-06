2e306f0d2ac93ef2bd34d96e12d8cc2f361cc031
rmotafreitas <rmotafreitas@gmail.com>
2024-10-07 00:09:48 +0100

    API Deploy

--- client/src/api/axiosConfig.ts
+++ client/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+  import.meta.env.VITE_API_BASE_URL || "https://x-ngmi-analyser.onrender.com";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
